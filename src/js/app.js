7c03f8347e8cc7fd0065ef5f1566a241e1a93a09
saifullah-N <n.mmsaifi16331@gmail.com>
2022-11-10 15:22:22 +0530

    myApp const in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -90,7 +90,8 @@ function update_object(dst, src, remove) {
     }
 }
 
-module.exports = new Vue ({
+
+const myApp = new Vue ({
     el: "body",
     
     data: function() {
@@ -434,3 +435,20 @@ module.exports = new Vue ({
         },
     },
 });
+
+const routes = [
+      { path: "/", component: lets-get-started },
+      //   { path: "/about", component: About },
+    ];
+
+    // 3. Create the router instance and pass the `routes` option
+    // You can pass in additional options here, but let's
+    // keep it simple for now.
+    const router = VueRouter.createRouter({
+      // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+      history: VueRouter.createWebHashHistory(),
+      routes, // short for `routes: routes`
+    });
+
+    myApp.options.router=router
+module.exports = myApp
