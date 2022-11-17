16ab2b1abfa5f725cf39ba7acdaef6e795dc3a91
saifullah-N <n.mmsaifi16331@gmail.com>
2022-11-17 19:24:23 +0530

    btn & get started

--- src/js/button-controller.js
+++ src/js/button-controller.js
@@ -1,13 +1,46 @@
-"use strict"
+// "use strict"
+
+// const api = require("./api");
+// const utils = require("./utils");
+// const merge = require("lodash.merge");
+
+
+// module.exports = {
+//   template: `
+
+//   `,
+//   // props: ["config"],
+//   data: function () {
+//     return {
+//       confirmReset: false,
+//       autoCheckUpgrade: true,
+//       button_type: "",
+//     };
+//   },
+//   methods: {
+//     set_button_type: function () {
+//       console.log("button fn called");
+//       try {
+//         api.put("/api/setButtonType", { " button_type": button_type });
+//         console.log("api called");
+//       } catch (error) {
+//         console.error("settings error:", error);
+//         alert("Cant set btn Type");
+//         return;
+//       }
+//     },
+//   },
+// };
+"use strict";
 
 const api = require("./api");
 const utils = require("./utils");
 const merge = require("lodash.merge");
 
-
 module.exports = {
   template: "#button-controller-view",
-  // props: ["config"],
+  props: ["config", "state"],
+
   data: function () {
     return {
       confirmReset: false,
@@ -15,16 +48,26 @@ module.exports = {
       button_type: "",
     };
   },
+
   methods: {
-  set_button_type:function(){
-    console.log("button fn called")
-    try {
-      api.put("/api/setButtonType", { " button_type": button_type });
-      console.log("api called")
-    } catch (error) {
-      console.error("settings error:", error);
-      alert("Cant set btn Type");
-      return;
-    }
-  }}
+
+    reset: async function () {
+      const config = {
+        button: button_type,
+      };
+
+      try {
+        // await api.put("config/save", config);
+        this.confirmReset = false;
+        this.$dispatch("update");
+        SvelteComponents.showDialog("Message", {
+          title: "Success",
+          message: "button type set",
+        });
+      } catch (error) {
+        console.error("Restore failed:", error);
+        alert("OOPS! an error has occured");
+      }
+    },
+  },
 };
