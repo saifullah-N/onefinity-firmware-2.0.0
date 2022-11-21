d3f8178c359fb53ebd5ff06857cb87a3b68280bc
saifullah-N <n.mmsaifi16331@gmail.com>
2022-11-22 00:47:59 +0530

    backend-/

--- src/py/bbctrl/Web.py
+++ src/py/bbctrl/Web.py
@@ -724,9 +724,9 @@ class ButtonTypeHandler(bbctrl.APIHandler):
         print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.",value)
         subprocess.Popen(['mount','-o','remount,rw','/boot','/config.txt'])        # subprocess.Popen(['echo',value["button"]])
         if (value["button"] == "rock"):
-            subprocess.Popen(['sed', '-i', "s@dtoverlay=gpio-poweroff,gpiopin=21@#dtoverlay=gpio-poweroff,gpiopin=21@", '/boot/config.txt'])
+            subprocess.Popen(['sed', '-i', "s/dtoverlay=gpio-poweroff,gpiopin=21/#rock", '/boot/config.txt'])
         elif(value["button"]=='push'):
-            subprocess.Popen( ['sed', '-i', "s@#dtoverlay=gpio-poweroff,gpiopin=21@dtoverlay=gpio-poweroff,gpiopin=21@", '/boot/config.txt'])
+            subprocess.Popen( ['sed', '-i', "s/#rock/dtoverlay=gpio-poweroff,gpiopin=21/", '/boot/config.txt'])
 # #             subprocess.Popen(['mount','-o','remount,rw','/boot'])
 # #         subprocess.Popen(['mount','-o','remount,ro','/boot'])
 # #     # subprocess.Popen(['mount -o remount,ro /boot'])
