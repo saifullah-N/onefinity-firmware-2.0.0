0a2290768cc191ff17724e4aec0d634146df663b
saifullah-N <n.mmsaifi16331@gmail.com>
2022-11-23 21:40:27 +0530

    check_call

--- src/py/bbctrl/Web.py
+++ src/py/bbctrl/Web.py
@@ -726,10 +726,8 @@ class ButtonTypeHandler(bbctrl.APIHandler):
             # subprocess.Popen(['sed', '-i', "s/dtoverlay=gpio-poweroff,gpiopin=21/#rock", '/boot/config.txt'])
             # subprocess.Popen([  'sed', '-i', "s/#shutdown/#rock", '/boot/config.txt'])
             # p=subprocess.Popen('sudo mount -o remount,rw /boot && sudo sed -i "s/dtoverlay=gpio-poweroff,gpiopin=21/#rock"  /boot/config.txt | sudo su', stdout=subprocess.PIPE, shell=True)
-            cmd = 'sudo mount -o remount,rw /boot && sudo sed -i "s/dtoverlay=gpio-poweroff,gpiopin=21/#rock"  /boot/config.txt | sudo su'
-            ps = subprocess.Popen(
-            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            output = ps.communicate()[0]
+            subprocess.check_call(
+                'sudo mount -o remount,rw /boot && sudo sed -i "s/dtoverlay=gpio-poweroff,gpiopin=21/#rock"  /boot/config.txt | sudo su', shell=True)
             # self.get_log().info("btn type set :"+ output.decode())
         elif(value["button"]=='push'):
             subprocess.Popen( ['sed', '-i', "s/#rock/dtoverlay=gpio-poweroff,gpiopin=21/", '/boot/config.txt'])
