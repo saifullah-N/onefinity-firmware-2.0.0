5db8d599969c181b024bdc4e32c803124fced280
saifullah-N <n.mmsaifi16331@gmail.com>
2022-12-11 18:49:35 +0530

    done

--- src/js/network.js
+++ src/js/network.js
@@ -1,14 +1,16 @@
 module.exports = {
-    template: "#network-view-template",
+  template: "#network-view-template",
+  props: ["render"],
 
-    attached: function() {
-        this.svelteComponent = SvelteComponents.createComponent(
-            "AdminNetworkView",
-            document.getElementById("admin-network")
-        );
-    },
+  attached: function () {
+    this.svelteComponent = SvelteComponents.createComponent(
+      "AdminNetworkView",
+      document.getElementById("admin-network")
+      );
+      this.render=true
+  },
 
-    detached: function() {
-        this.svelteComponent.$destroy();
-    }
+  detached: function () {
+    this.svelteComponent.$destroy();
+  },
 };
