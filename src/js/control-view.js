5f32c3ea43966256b3608321c32ceabb878a154b
saifullah-N <n.mmsaifi16331@gmail.com>
2022-12-11 22:50:24 +0530

    axis,path

--- src/js/control-view.js
+++ src/js/control-view.js
@@ -47,7 +47,7 @@ module.exports = {
 
   components: {
     "axis-control": require("./axis-control"),
-    // "path-viewer": require("./path-viewer"),
+    "path-viewer": require("./path-viewer"),
     // "gcode-viewer": require("./gcode-viewer"),
   },
 
