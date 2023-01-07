27a5de94dd6c30ee8dca83b8d0f1a5d6ec8e3a0b
saifullah-N <n.mmsaifi16331@gmail.com>
2023-01-07 16:07:21 +0530

    z-slider setup

--- src/js/z-slider-view.js
+++ src/js/z-slider-view.js
@@ -4,18 +4,18 @@ const api = require("./api");
 const utils = require("./utils");
 const merge = require("lodash.merge");
 
+const config_defaults = require("../resources/onefinity_defaults.json");
+
 const z_slider_defaults = {
   "Z-16 Original": {
     "travel-per-rev": 4,
-    "max-accel": 3,
-    "max-soft-limit": -133,
+    "min-soft-limit": -133,
   },
   "Z-20 Heavy Duty": {
     "travel-per-rev": 10,
-    "max-accel": 7,
-    "max-soft-limit": -160,
+    "min-soft-limit": -160,
   },
-};
+}; 
 
 module.exports = {
   template: "#z-slider-view-template",
@@ -26,7 +26,6 @@ module.exports = {
       autoCheckUpgrade: true,
       z_slider: "",
       z_slider_variant: " ",
-      // config: "",
     };
   },
 
@@ -55,22 +54,28 @@ module.exports = {
     // },
 
     set_z_slider: async function () {
-      // const z_variant = merge(
-      //   {},
-      // this.config.motors[3],
-      //   z_slider_defaults[this.z_slider_variant]
-      // );
+                
+                
+        const z_variant = merge({}, 
+          this.config.motors[3],
+          z_slider_defaults[this.z_slider_variant]);
 
-      // this.config.motors[3] = z_variant;
+        const config = merge(
+          {},
+        this.config["motors"],
+        config_defaults,
+      );
+
+       config.motors[3] = z_variant;
       try {
-        console.log(this.config);
-        //   await api.put("config/save", this.config);
-        //   this.$dispatch("update");
-        //   SvelteComponents.showDialog("Message", {
-        //     title: "Success",
-        //     message: "Configuration restored",
-        //   });
-        //   this.z_slider = false;
+        console.table(config);
+          await api.put("config/save",config);
+          this.$dispatch("update");
+          SvelteComponents.showDialog("Message", {
+            title: "Success",
+            message: "Configuration restored",
+          });
+          this.z_slider = false;
       } catch (error) {
         console.error("Z slider failed:", error);
         alert("failed to set Z slider  ");
